52d551719194f270c1a9c1de52bd4f867cf61f55
David Requeno <davidrequeno52@gmail.com>
2024-05-02 21:56:32 -0600

    ...

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
-import { reversePrefix } from "./day1.js";
+import { twoSum } from "./day2.js";
 // dia 1
-console.log(reversePrefix("abcdefd", "d"));
+/*console.log(reversePrefix("abcdefd", "d"));
 console.log(reversePrefix("xyxzxe", "z"));
-console.log(reversePrefix("abcd", "z"));
+console.log(reversePrefix("abcd", "z"));  */
+// dia 2
+const nums = [2, 7, 11, 15];
+const target = 9;
+const nums2 = [3, 2, 4];
+const target2 = 6;
+const nums3 = [3, 3];
+const target3 = 6;
+console.log(twoSum(nums, target));
+console.log(twoSum(nums2, target2));
+console.log(twoSum(nums3, target3));
 //# sourceMappingURL=index.js.map
