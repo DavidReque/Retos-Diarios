7cb3309984af9fc717052c02d8c0ee6bd159e03f
David Requeno <davidrequeno52@gmail.com>
2024-05-13 20:46:45 -0600

    #

--- python/day8.py
+++ python/day8.py
@@ -10,15 +10,23 @@ class Solution:
             'M': 1000
         } 
         
+        #variable total para almacenar el valor total del número romano
         total = 0
+        #prev_value para almacenar el valor del símbolo romano anterior
         prev_value = 0
 
         for char in s:
+            #valor del caracter actual del diccionario
             value = roman_values[char]
+            # Si el valor del caracter actual es mayor que el valor del caracter anterior,
+            # significa que estamos tratando con un caso como IV o IX, donde la resta se aplica
             if value > prev_value:
+                # Restamos dos veces el valor anterior del valor actual y lo agregamos al total
                 total += value - 2 * prev_value
             else:
+                # Si no estamos en un caso de resta, simplemente sumamos el valor al total
                 total += value
+            # Actualizamos el valor previo para el próximo caracter
             prev_value = value
             
         return total
