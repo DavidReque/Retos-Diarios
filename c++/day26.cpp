372907989ecb4d3a7bfa237eee95e150384d9a5d
David Requeno <davidrequeno52@gmail.com>
2024-06-20 21:11:02 -0600

    day 26

---
+++ c++/day26.cpp
@@ -0,0 +1,61 @@
+/*Dado un array entero nums de longitud n y un número entero target, encuentra tres enteros en nums de tal manera que la suma esté más cerca de target.
+
+Devolución la suma de los tres enteros.
+
+Puede suponer que cada entrada tendría exactamente una solución.
+
+Ejemplo 1:
+
+Entrada: nums = [-1,2,1,-4], target = 1
+Salida: 2
+Explicación: La suma más cercana al objetivo es 2. (-1 + 2 + 1 = 2).
+Ejemplo 2:
+
+Entrada: nums = [0,0,0], target = 1
+Salida: 0
+Explicación: La suma más cercana al objetivo es 0. (0 + 0 + 0 = 0).*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        // Ordenar el array
+        sort(nums.begin(), nums.end());
+        // Inicializar closest_sum con un valor arbitrario grande
+        int closest_sum = nums[0] + nums[1] + nums[2];
+
+        // iterar sobre cada elemento excepto sobre los dos ultimos
+        for (int i = 0; i < nums.size() - 2; i++)
+        {
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            // Usar dos punteros para encontrar la suma más cercana al target
+            while (left < right)
+            {
+                int current_sum = nums[i] + nums[left] + nums[right];
+                // Actualizar closest_sum si current_sum está más cerca del target
+                if (abs(current_sum - target) < abs(closest_sum - target))
+                {
+                    closest_sum = current_sum;
+                }
+
+                // Mover los punteros dependiendo de la comparación con el target
+                if (current_sum < target)
+                {
+                    ++left;
+                } else
+                {
+                    --right;
+                }
+            }
+        }
+
+        return closest_sum;
+    }
+};
